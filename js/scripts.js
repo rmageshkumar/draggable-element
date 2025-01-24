de2244964ee1fc38c9c48fbddca05065dee56edd
Mageshkumar R <mageshkumar.it@gmail.com>
2025-01-24 17:58:33 +0000

    draggable evenets

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,33 @@ const isTouchDevice = () => {
     return false;
   }
 };
+
+isTouchDevice();
+
+draggableElement.addEventListener(events[deviceType].down, (e) => {
+  e.preventDefault();
+
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  moveElement = true;
+});
+
+draggableElement.addEventListener(events[deviceType].move, (e) => {
+  e.preventDefault();
+
+  if (moveElement) {
+    currentX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+    currentY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+    draggableElement.style.top = `${
+      draggableElement.offsetTop + (currentY - initialY)
+    }px`;
+    draggableElement.style.left = `${
+      draggableElement.offsetLeft + (currentX - initialX)
+    }px`;
+
+    initialX = currentX;
+    initialY = currentY;
+  }
+});
